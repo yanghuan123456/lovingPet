5f8350959842091d414cc6f8a1a3efc932d641ee
lianjian112 <1475779091@qq.com>
2018-05-02 12:57:26 +0800

    商品管理

--- petProject/node.js/routes/service.js
+++ petProject/node.js/routes/service.js
@@ -3,16 +3,21 @@ var router = express.Router();
 const db = require('ykt-mongo');
 const hc = require("ykt-http-client");
 const multiparty = require('multiparty');
-<<<<<<< HEAD
-// hc.url("192.168.43.202:3001");
-hc.url("127.0.0.1:3001");
+
+// hc.url("192.168.43.202:3001 
+
+
+// hc.url("127.0.0.1:3001");
+hc.url("192.168.43.143:3001");
 
 /* GET users listing. */ //二级路由
 router.get('/', function (req, res, next) {
   let page=req.query.page||1;
   let rows=req.query.rows||5;
   let value=req.query.value;
-  let id=req.query.id;
+  let id=req.query.id 
+
+;
   if(value!=null){
     hc.get("/services/",{"stores":value,page,rows,"users.$id":id}).then(function (data) {
         res.send(data);
@@ -26,31 +31,25 @@ router.get('/', function (req, res, next) {
 //post提交
 router.post('/', function (req, res, next) {
   let stores = req.body.stores; //post是body提交
-  let name=req.body.name;
+  let name=req.body.name 
+
+;
   let king=req.body.king;
   let money=req.body.money;
   let time= req.body.time;
   let img=req.body.img;
-  let id=req.body.id
+  let id=req.body.id 
+
   hc.post("/services",{stores,name,king,money,time,img,"users":JSON.stringify({$ref:"users",$id:id})}).then(function(data){
     res.send("suc");
-  });
-=======
-const path = require('path');
-const hc = require('ykt-http-client');
-
-// hc.url("127.0.0.1:3001");
-hc.url("192.168.43.143:3001");
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('services', { title: 'Express' });
->>>>>>> 680c5b37f7d0af53e733ded374d073ca9a0c8c7e
-});
+  })
+})
 
 //删除
 router.delete("/:id", function(req, res, next){
-  hc.delete("/services/"+req.params.id).then(function(data){
+  hc.delete("/services/"+req.params.id 
+
+).then(function(data){
       res.send("suc");
   });
 });
@@ -70,12 +69,16 @@ router.post("/upload",function(req,res,next){
 // 修改
 router.put('/:id', function (req, res, next){
   let stores=req.body.stores;
-  let name=req.body.name;
+  let name=req.body.name 
+
+;
   let king=req.body.king;
   let money=req.body.money;
   let time=req.body.time;
   let img=req.body.img
-  hc.put("/services/"+req.params.id,{stores,name,money,king,time,img}).then(function(data){
+  hc.put("/services/"+req.params.id 
+
+,{stores,name,money,king,time,img}).then(function(data){
     res.send("suc");
   });
 })
