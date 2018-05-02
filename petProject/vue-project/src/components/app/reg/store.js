87a4eadcc09a3403dbd21a4e5a68185b0ffc1c09
maluping666 <773823508qq@.com>
2018-05-02 10:56:10 +0800

    新增图片的修改

--- petProject/vue-project/src/components/app/reg/store.js
+++ petProject/vue-project/src/components/app/reg/store.js
@@ -24,9 +24,7 @@ export default {
     },
     actions: {
         async getUser(context,obj) {
-            console.log(obj)
             const {data} = await axios.post(`/users/up`,obj)
-            console.log(data)
             context.commit('setData',data)
         },
         async queryDataType(context, obj ) {
